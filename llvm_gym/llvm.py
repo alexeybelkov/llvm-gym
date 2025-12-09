6fac0b93db9b0fcbda3ba737caffbd8ee935d6ff
alexeybelkov <fpmbelkov@gmail.com>
2025-12-09 13:14:02 +0000

    llvm wrapper

--- llvm_gym/llvm.py
+++ llvm_gym/llvm.py
@@ -3,3 +3,10 @@ from pathlib import Path
 class LLVMWrapper:
 	def __init__(self, llvm_path: Path|str):
 		self.llvm_path = Path(llvm_path)
+	def run_command(self, command: str):
+		pass
+	def opt(self, *args):
+		pass
+	@property
+	def version(self) -> str:
+		return '15.0.4'
